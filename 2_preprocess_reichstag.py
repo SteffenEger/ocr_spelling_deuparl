3fa8f198399b2b0c517bdad3d73ee1c9846e0bb7
JaSilasJa <48722456+JaSilasJa@users.noreply.github.com>
2021-12-14 16:53:11 +0100

    Update 2_preprocess_reichstag.py
    add comments

--- 2_preprocess_reichstag.py
+++ 2_preprocess_reichstag.py
@@ -7,41 +7,47 @@ import tqdm as tqdm
 
 
 def newLine(sentance, dotAt):
+    # abbreviations of words with one letter
     if (dotAt > 1 and (sentance[dotAt- 2] == ' ' or sentance[dotAt- 2] == '.')) or dotAt == 1:
         word1 = sentance[dotAt- 1:dotAt]
         if word1 == "a" or word1 == "b" or word1 == "d" or word1 == "i" or word1 == "K" or word1 == "m" or word1 == "s" or word1 == "S" or word1 == "u" or word1 == "v" or word1 == "z" or word1 == "Z":
             return False
-            
+    # abbreviations of words with two letter    
     if (dotAt > 2 and sentance[dotAt - 3] == ' ') or dotAt == 2:
         word2 = sentance[dotAt - 2:dotAt]
         if word2 == "ca" or word2 == "rc" or word2 == "re" or word2 == "Fl" or word2 == "Dr" or word2 == "St" or word2 == "Fr" or word2 == "Kr" or word2 == "Pr":
             return False
-
+    # abbreviations of words with three letter
     if (dotAt > 3 and sentance[dotAt- 4] == ' ') or dotAt == 3:
         word3 = sentance[dotAt- 3:dotAt]
         if word3 == "Bew" or word3 == "Grf" or word3 == "Str" or word3 == "Inf" or word3 == "Reg" or word3 == "Grs" or word3 == "Kgr":
             return False
-
+    # abbreviations of words with four letter
     if (dotAt > 4 and sentance[dotAt- 5] == ' ') or dotAt == 4:
         word4 = sentance[dotAt- 4:dotAt]
         if word4 == "Prov" or word4 == "Thlr":
             return False
-
+    # abbreviations of words with five letter
     if (dotAt > 5 and sentance[dotAt- 6] == ' ') or dotAt == 5:
         word5 = sentance[dotAt- 5:dotAt]
         if word5 == "Preuß":
             return False
-
+    # no linebreak before - and :
     if (len(sentance) - dotAt) > 1 and sentance[dotAt + 1] == '-'  and sentance[dotAt + 1] == ':':
         return False
+    # no linebreak before digit like 1.2
     if re.search('[0-9]', sentance[dotAt - 1]) or re.search('[0-9]', sentance[dotAt - 2]):
         return False
+    # linebreak by '.Word' oder ..
     if (len(sentance) - dotAt) > 1 and (sentance[dotAt + 1].isupper() or sentance[dotAt + 1] == '.'):
         return True
+    # linebreak by '. Word'
     if (len(sentance) - dotAt) > 2 and sentance[dotAt + 2].isupper():
         return True
+    # linebreak at end of line
     if (len(sentance) - 1 == dotAt):
         return True
+    # else  no linebreak
     return False
 
 if __name__ == "__main__":
@@ -133,19 +139,21 @@ if __name__ == "__main__":
                 lines_tokens = [[tok.lower() for tok in line.split()]
                             for line in lines]
 
-                #"""
+                #split text in sentances
                 a = 0
                 lines_fix, buffer = [], []
                 for line in lines:
                     for c  in range (len (line)): 
-
+                        # for listings
                         if c == 0 and line[0].isdigit():
                             lines_fix.append("".join(buffer))
                             buffer = []
 
+                        # line is part of a santance that alrady started
                         elif c == len(line) -1 and (line[c] != '.' and line[c] != '?' and line[c] != '!'):
                             buffer.append(line[a - len(line) - 1:] + " ")
                         
+                        # found a dot and checks if it needs to be splitted
                         elif (line[c] == '.' or line[c] == '?' or line[c] == '!') and newLine(line, c):
                             buffer.append(line[a - len(line) - 1: c + 1])
                             lines_fix.append("".join(buffer))
@@ -156,6 +164,7 @@ if __name__ == "__main__":
                 lines = lines_fix
                 #"""
 
+                #deletes uses liens
                 lines = [line for line in lines if re.search('[a-zA-Z]', line)]
 
                 count_per_bucket[bucket_name] += 1
