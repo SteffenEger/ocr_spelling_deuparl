72e207a68c6d29ffde02c504ca3e73f0f808f3a3
long21wt <46249208+long21wt@users.noreply.github.com>
2022-01-17 14:26:38 +0100

    preprocessing: Rework Reichstag (#3)
    * Update 2_preprocess_reichstag.py
    
    Split Text into sentences.
    
    Still not perfect
    
    * Update 2_preprocess_reichstag.py
    
    Little fix with listings
    
    * Update 2_preprocess_reichstag.py
    
    add comments
    
    * preprocessing of reichstag again
    
    List handling and each line per sentence
    
    tables are far from ok and useless atm
    
    Co-authored-by: JaSilasJa <48722456+JaSilasJa@users.noreply.github.com>

--- 1_collect_reichstag.py
+++ 1_collect_reichstag.py
@@ -50,58 +50,11 @@ if __name__ == "__main__":
                     # separating text into lines
                     if char_param.text == None:
                         char_param.text = " "
-                    # if char_param.text in punctuation:
-                    #     char_param.text = char_param.text + " "
                     s.append(char_param.text)
                 lines.append("".join(s))
 
         text = "\n".join(lines)
 
-        ######################################################################################
-        # source: https://github.com/SteffenEger/Corpus/blob/master/Text_U%CC%88berarbeitet.py
-        ######################################################################################
-        #
-        # # concatenates lines based on "-", better to do this in postprocessing after OCR post-correction
-        #
-        # new_lines = []
-        # prevLine = ""
-        # for line in lines:
-        #     line = line.strip()
-        #     if line[-1] == "¬" or line[-1] == "-":
-        #         prevLine = prevLine + line[:-1]
-        #     else:
-        #         new_lines.append(prevLine + line)
-        #         prevLine = ""
-        # lines = new_lines
-        # text = "\n".join(lines)
-
-        #############################################################################
-        # https://github.com/SteffenEger/Corpus/blob/master/Additional_Corrections.py
-        #############################################################################
-        #
-        # # applies some text normalization (not necessary and not working)
-        #
-        # def text_normalization(raw_data):
-        #     word_list = []
-        #     numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-        #     sent_text = nltk.sent_tokenize(raw_data)
-        #     for sent in sent_text:
-        #         for word in nltk.word_tokenize(sent):
-        #             if len(word) > 2 and word[0] in numbers and word[1] not in numbers:
-        #                 word_list.append(word[0])
-        #                 word_list.append(" ")
-        #                 word_list.append(word[1:])
-        #             else:
-        #                 word_list.append(word)
-        #         print(" ".join(word_list))
-        #         word_list = []
-        #
-        #     return word_list
-        #
-        #
-        # def sents_creation(data):
-        #     sent_text = nltk.sent_tokenize(data)
-        #     return sent_text
 
         idx_per_year[year] += 1
         if not os.path.isdir(f"data/1_collected/Reichstag/{year}/"):
--- 2_preprocess_reichstag.py
+++ 2_preprocess_reichstag.py
@@ -5,6 +5,84 @@ import re
 
 import tqdm as tqdm
 
+pattern = re.compile(r"""   
+                                ^M{0,3}
+                                (CM|CD|D?C{0,3})?
+                                (XC|XL|L?X{0,3})?
+                                (IX|IV|V?(I|i|H){0,3})?$
+            """, re.VERBOSE)
+
+
+def lineStartsWithNumber(line):
+    if line[0].isdigit() or (len(line) > 1 and line[0] in ['L', 'Z'] and line[1] == '.'):
+        return True
+    if (line[0].islower() and len(line) > 2 and line[1] == '.' and line[0] != 'v'):
+        return True
+    #if (line.startswith("Art.")):
+    #    return True
+    if re.match(pattern, line.split(' ')[0].replace(".", "")):
+        return True
+    if len(line) > 2 and line[0] == '.' and line[1].isdigit():
+        return True
+    return False
+
+abbreviationsStart = ['.', '-', ' ']
+words1 = ["a", "b", "d", "i", "K", "m", "s", "S", "u", "v", "z", "Z", 'D']
+
+def newLine(sentance, dotAt):
+    if (line[c] not in ".?!;"):
+        return False
+    # abbreviations of words with one letter
+    if dotAt > 1 and (sentance[dotAt - 2] in abbreviationsStart) or dotAt == 1:
+        word1 = sentance[dotAt - 1:dotAt]
+        if word1 in word1:
+            return False
+    # abbreviations of words with two letter
+    if (dotAt > 2 and sentance[dotAt - 3] in abbreviationsStart) or dotAt == 2:
+        word2 = sentance[dotAt - 2:dotAt]
+        if word2 in ["ca", "rc", "re", "Fl", "Dr", "St", "Fr", "Kr", "Pr", "Sr"]:
+            return False
+    # abbreviations of words with three letter
+    if (dotAt > 3 and sentance[dotAt - 4] in abbreviationsStart) or dotAt == 3:
+        word3 = sentance[dotAt - 3:dotAt]
+        if word3 in ["Bew", "Grf", "Str", "Inf", "Reg", "Grs", "Kgr", "Bzg", "dir"]:
+            return False
+    # abbreviations of words with four letter
+    if (dotAt > 4 and sentance[dotAt - 5] in abbreviationsStart) or dotAt == 4:
+        word4 = sentance[dotAt - 4:dotAt]
+        if word4 in ["Prov", "Thlr"]:
+            return False
+    # abbreviations of words with five letter
+    if (dotAt > 5 and sentance[dotAt - 6] in abbreviationsStart) or dotAt == 5:
+        word5 = sentance[dotAt - 5:dotAt]
+        if word5 in ["Preuß", "indir"]:
+            return False
+    # no linebreak before - and :
+    if (len(sentance) - dotAt) > 1 and sentance[dotAt + 1] == '-' and sentance[dotAt + 1] == ':':
+        return False
+    # no linebreak before digit like 1.2
+    if re.search('[0-9]', sentance[dotAt - 1]) or re.search('[0-9]', sentance[dotAt - 2]):
+        return False
+    # linebreak by '.Word' oder ..
+    if (len(sentance) - dotAt) > 1 and (sentance[dotAt + 1].isupper() or sentance[dotAt + 1] == '.'):
+        return True
+    # linebreak by '. Word'
+    if (len(sentance) - dotAt) > 2 and sentance[dotAt + 2].isupper():
+        return True
+    # linebreak at end of line
+    if (len(sentance) - 1 == dotAt):
+        return True
+    # else  no linebreak
+    return False
+
+
+def removeOne(sentance, oneAt):
+    if (oneAt < len(sentance) - 1 and sentance[oneAt + 1].isalpha()):
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
 
     bucket_folders = list(sorted(glob.glob("data/1_collected/Reichstag/*/")))
@@ -23,17 +101,18 @@ if __name__ == "__main__":
             with open(file_path, encoding="utf-8") as file:
                 lines = file.readlines()
 
-                # delete strange lines from the beginning 
+                # delete strange lines from the beginning
                 startline = 0
                 if bucket_name == 1867:
                     for line in lines:
-                        if "Kirth's" or "Kirth's" not in line:
+                        if "Kirth's" not in line or "Kirth's" not in line:
                             startline += 1
                         else:
                             break
-
-                if startline != len(lines):
-                    lines = lines[startline:]
+                    if startline != len(lines):
+                        lines = lines[startline:]
+                else:
+                    break
 
                 # fix splitted words at the end of a line
                 # ... Bundes-
@@ -58,7 +137,7 @@ if __name__ == "__main__":
                 # remove double spaces
                 lines = [re.sub('\s\s+', ' ', line).strip() for line in lines]
 
-                # remove noisy digits
+                # remove noisy digits TODO first letter of a line like $ in "$ Test geht weiter"
                 # 'Remove digits that are appending, prepending or in the middle of some words, e.g. because of faulty OCR'
                 lines = [re.sub(r'\b\d(?P<quote>[A-Za-zßäöüÄÖÜ]+)\b',
                                 '\g<quote>', line) for line in lines]
@@ -74,6 +153,9 @@ if __name__ == "__main__":
                 lines = [re.sub(r'^(-|—|–|\s)+\s', '', line) for line in lines]
                 lines = [re.sub(r'\b\s(-|—|–|\s)+$', '', line)
                          for line in lines]
+                # remove some random chars
+                lines = [re.sub(r'(<|>|\^|«|»|\*)', ' ', line)
+                         for line in lines]
 
                 # remove linebreaks
                 lines = [re.sub(r'[\n\t]', '', line).strip() for line in lines]
@@ -81,16 +163,89 @@ if __name__ == "__main__":
                 # replace digits
                 #lines = [re.sub(r'\d+', ' 0 ', line).strip() for line in lines]
 
+                # removes ... #TODO replace with other place holder
+                lines = [re.sub(r'\.\.\.', ' ', line).strip() for line in lines]
+
+                # reduce 
+                #lines = [ re.sub(r'(\.| ){3}', ' \n', line).strip() for line in lines]
+                lines_fix, buffer = [], []
+                for line in lines:
+                    line = re.sub(r'(\.| ){3}', ' $', line).strip()
+                    for a in line.split("$"):
+                        lines_fix.append("".join(a))
+                
+                lines = lines_fix
+
                 # reduce numerical sequences
-                lines = [re.sub(r'((0)\s?){2,}', '\\2 ', line).strip() for line in lines]
+                #lines = [ re.sub(r'((0)\s?){2,}', '\\2 ', line).strip() for line in lines]
 
                 # filter doc
                 lines = [line for line in lines if len(line) > 1]
+                lines = [line for line in lines if line.count('/') < 4]
+                lines = [line for line in lines if line.count('_') < 2]
 
                 # lowercase
                 lines_tokens = [[tok.lower() for tok in line.split()]
-                            for line in lines]
+                                for line in lines]
 
+                
+                # split text in sentances
+                a = 0
+                lines_fix, buffer = [], []
+                for line in lines:
+
+                    for c in range(len(line)):
+
+                        if line[c] == '1' and removeOne(line, c):
+                            string_list = list(line)
+                            string_list[c] = " "
+                            line = "".join(string_list)
+
+                        # for listings c == 0 and
+                        if c == len(line) - 1 and lineStartsWithNumber(line):
+                            lines_fix.append("".join(buffer))
+                            buffer = []
+                            buffer.append(line)
+                            # break
+
+                        elif lineStartsWithNumber(line):
+                            continue
+
+                        # line is part of a santence that alrady started
+                        elif c == len(line) - 1 and not newLine(line, c):
+                            buffer.append(line[a - len(line) - 1:] + " ")
+
+                        # found a dot and checks if it needs to be splitted
+                        elif newLine(line, c):
+                            buffer.append(line[a - len(line) - 1: c + 1])
+                            lines_fix.append("".join(buffer))
+                            buffer = []
+                            a = c + 2
+                    a = 0
+
+                lines = lines_fix
+                # """
+                
+                # puts smal lines together
+                minLineLen = 10
+                lines_fix, buffer = [], []
+                for line in lines:
+                    if buffer == []:
+                        buffer.append(line)
+                    elif len(line) < minLineLen:
+                        buffer.append(line)
+                    else:
+                        lines_fix.append("".join(buffer))
+                        buffer = []
+                        buffer.append(line)
+
+                lines = lines_fix
+
+                # deletes useless liens
+                lines = [line for line in lines if re.search('[a-zA-Z]', line)]
+
+                # remove not needed white spaces
+                lines = [re.sub('\s\s+', ' ', line).strip() for line in lines]
 
                 count_per_bucket[bucket_name] += 1
                 with open(f"data/2_preprocessed/Reichstag/{bucket_name}/{count_per_bucket[bucket_name]}.txt", "w", encoding="utf-8") as file:
